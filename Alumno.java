1180adc7b263dc5a3d756e595caa576b854f1a2d
DidierSaho <95487810+DidierSaho@users.noreply.github.com>
2024-06-16 18:21:19 -0300

    Update Alumno.java

--- Alumno.java
+++ Alumno.java
@@ -4,18 +4,16 @@ public class Alumno {
     private String nombre;
     private int legajo;
 
-    // constructor que recibe el nombre y el legajo como parámetros
     public Alumno(String nombre, int legajo) {
-        this.nombre = nombre;  // asigna los parámetros a los atributos de la clase
-        this.legajo = legajo;  // asigna los parámetros a los atributos de la clase
+        this.nombre = nombre;  
+        this.legajo = legajo;
     }
 
-    // metodo getter para obtener el nombre del alumno
+
     public String getNombre() {
         return nombre;
     }
 
-    // método getter para obtener el legajo del alumno
     public int getLegajo() {
         return legajo;
     }
