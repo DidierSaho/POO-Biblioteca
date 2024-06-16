ebc55392e62c7847a2afb8ce6d22b875ad4445b5
DidierSaho <95487810+DidierSaho@users.noreply.github.com>
2024-06-16 18:23:10 -0300

    Update Libro.java

--- Libro.java
+++ Libro.java
@@ -11,22 +11,22 @@ public class Libro {
         this.autor = autor;
     }
 
-    // metodo getter para obtener el título del libro
+   
     public String getTitulo() {
         return titulo;
     }
 
-    // método getter para obtener el autor del libro
+   
     public String getAutor() {
         return autor;
     }
 
-    // método para verificar si el libro esta disponible
+    
     public boolean estaDisponible() {
         return disponible;
     }
 
-    // método para establecer la disponibilidad del libro
+   
     public void setDisponibilidad(boolean estado) {
         this.disponible = estado;
     }
