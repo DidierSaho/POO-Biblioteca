040da112b78f3423aa4e0bb439a48d49ee08ffa1
DidierSaho <95487810+DidierSaho@users.noreply.github.com>
2024-06-16 18:25:23 -0300

    Update Prestamo.java

--- Prestamo.java
+++ Prestamo.java
@@ -6,13 +6,13 @@ public class Prestamo {
 
     // constructor que recibe un libro y un alumno como parámetros
     public Prestamo(Libro unLibro, Alumno unAlumno) throws MiExcepcion {
-        if (unLibro == null || unAlumno == null) {   // verifica si el libro o el alumno son nulos
-            throw new MiExcepcion("El libro o el alumno no pueden ser nulos"); // excepción si el libro o el alumno son nulos
+        if (unLibro == null || unAlumno == null) {  
+            throw new MiExcepcion("El libro o el alumno no pueden ser nulos"); 
         }
-        this.libro = unLibro; // asigna los parámetros a los atributos de la clase
-        this.alumno = unAlumno; // asigna los parámetros a los atributos de la clase
+        this.libro = unLibro; e
+        this.alumno = unAlumno;
 
-        unLibro.setDisponibilidad(false); // marca el libro como no disponible
+        unLibro.setDisponibilidad(false); 
     }
 
     // metodo getter para obtener el libro asociado al préstamo
@@ -25,9 +25,9 @@ public class Prestamo {
         return alumno;
     }
 
-    // clase estática que define una excepción 
+    
     public static class MiExcepcion extends Exception {
-        public MiExcepcion(String mensaje) { // constructor de la excepción 
+        public MiExcepcion(String mensaje) { 
             super(mensaje);
         }
     }
